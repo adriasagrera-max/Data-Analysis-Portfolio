fb26d527e92ef2463a2f29911c3ffc820d0b3488
Adrià Sagrera <adriasagrera@gmail.com>
2026-01-20 13:44:44 +0100

    Add files via upload

--- Consultas_sql/Consultas.sql
+++ Consultas_sql/Consultas.sql
@@ -119,3 +119,71 @@ GROUP BY
     PD.HomeOwnerFlag,
     PD.NumberCarsOwned
     ORDER BY CustomerID
+    
+    
+    
+    
+    -- CONSULTA NÚMERO 3
+    
+    SELECT 
+	SUM(sog.SubTotal) AS TotalAmount,
+	IFNULL(BikePurchase.BikePurchase, 0) AS BikePurchase,
+	sog.CustomerID, 
+	sst.Name AS Country,
+	sst.CountryRegionCode,
+	sst.Group,
+	sc.PersonID,
+	pp.PersonType,
+	vpd.DateFirstPurchase,
+	vpd.BirthDate, 
+	TIMESTAMPDIFF(year,vpd.BirthDate, NOW()) AS Age,
+	vpd.MaritalStatus, 
+	vpd.YearlyIncome, 
+	vpd.Gender, 
+	vpd.TotalChildren, 
+	vpd.Education, 
+	vpd.Occupation, 
+	vpd.HomeOwnerFlag, 
+	vpd.NumberCarsOwned
+FROM SalesOrderHeader AS sog
+	LEFT JOIN Customer AS sc 
+		ON sc.CustomerID = sog.CustomerID 
+	INNER JOIN Person AS pp 
+		ON pp.BusinessEntityID=sc.PersonID
+	LEFT JOIN SalesTerritory AS sst 
+		ON sst.TerritoryID = sOG.TerritoryID
+	LEFT JOIN  vpersondemopgraphics AS vpd 
+		ON vpd.BusinessEntityID = sc.PersonID 
+	LEFT JOIN (			
+				SELECT DISTINCT soh.CustomerID,
+								1 AS BikePurchase
+				FROM SalesOrderDetail AS sod
+					INNER JOIN SalesOrderHeader AS SOh 
+						ON soH.SalesOrderID = sod.SalesOrderID
+					INNER JOIN Customer AS sc 
+						ON sc.CustomerID = soh.CustomerID 
+					INNER JOIN Product AS pp 
+						ON pp.ProductID = sod.ProductID
+					INNER JOIN ProductSubcategory AS pps 
+						ON pps.ProductSubCategoryID = pp.ProductSubcategoryID
+				WHERE pps.ProductSubcategoryID IN (1,2,3)
+				) AS BikePurchase ON BikePurchase.CustomerID = sog.CustomerID
+WHERE pp.PersonType = 'IN'
+GROUP BY BikePurchase.BikePurchase, 
+		sog.CustomerID, 
+		sst.Name,
+		sst.CountryRegionCode,
+		sst.Group,
+		sc.PersonID,
+		pp.PersonType,
+		vpd.DateFirstPurchase,
+		vpd.BirthDate, 
+		vpd.MaritalStatus, 
+		vpd.YearlyIncome, 
+		vpd.Gender, 
+		vpd.TotalChildren, 
+		vpd.Education, 
+		vpd.Occupation, 
+		vpd.HomeOwnerFlag, 
+		vpd.NumberCarsOwned
+ORDER BY vpd.DateFirstPurchase ASC;
